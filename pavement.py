843640b35e363fc292056d2e4d3aa490a1b02fa4
Max Kaye <m@xk.io>
2014-01-30 09:55:52 +1100

    fix pavement

--- pavement.py
+++ pavement.py
@@ -149,7 +149,7 @@ def sync(options):
     Run the syncdb and migrate management commands to create and migrate a DB
     """
     # Commented syncdb line to avoid loaddata issues
-    #sh("python manage.py syncdb --all --noinput")
+    sh("python manage.py syncdb --all --noinput")
     #sh("python manage.py migrate --noinput")
     sh("python manage.py loaddata sample_admin.json")
 
